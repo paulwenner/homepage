08c5e0b978e8c71b2d4f13fdfe92649887e4bd9e
paulwenner <141442554+paulwenner@users.noreply.github.com>
2024-01-04 23:58:32 +0100

    more updating

--- static/script.js
+++ static/script.js
@@ -1,16 +1,1 @@
-window.onload = () => {
-
-    const correctbutton = document.querySelector("#Correct");
-    const wrongbutton = document.querySelector("#Incorrect");
-
-    correctbutton.addEventListener("click", () => {
-        $('#quizModal').modal('hide');
-    })
-
-}
-
-
-$(document).ready(function () {
-    $('#quizModal').modal('show');
-});
 
